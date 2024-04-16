e592c58f2a683739e454714741280ce3272055c8
floatyyyy <imflappyxd@gmail.com>
2024-04-16 11:13:41 +0200

    update

--- Main.py
+++ Main.py
@@ -7,23 +7,20 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 #Fish
-playerimg = pygame.image.load('player.png')
 Pos = v.Vector(370, 480)
 Vel = v.Vector(0, 0)
 
 f.Fisk(Pos,Vel,screen)
 
 # Update and draw the fish
-fish = f.Fisk(Pos, Vel,)
+fish = f.Fisk(Pos, Vel,screen)
 fish.update()     
 fish.draw()
 
 running = True
 while running:
    for event in pygame.event.get():
-      if event.type == pygame.QUIT():
+      if event.type == pygame.QUIT:
          running = False
 
 pygame.display.update()
-
-pygame.quit()
